35bb15112b416bffab4275e9d58725514d11e6eb
Mohit Mohan <mht.mhn@gmail.com>
2015-03-12 22:45:16 +0530

    Indented Code and Second Test Success!
    actuate() running servo smoothly, made the code look legible

--- libraries/GCRA/GCRA.cpp
+++ libraries/GCRA/GCRA.cpp
@@ -1,106 +1,129 @@
 /*
-----------------------------------------
-https://github.com/sinisterdev/GCRA_MKI
-----------------------------------------
-S1N1S73RDevelopers-03/10/2015
-I invest time and resources providing 
-this open source code, Please provide 
-proper citations if you plan use it in 
-any of your projects. It may benefit 
-someone else as it did to you!
-----------------------------------------
+	----------------------------------------
+	https://github.com/sinisterdev/GCRA_MKI
+	----------------------------------------
+	S1N1S73RDevelopers-03/10/2015
+	I invest time and resources providing 
+	this open source code, Please provide 
+	proper citations if you plan use it in 
+	any of your projects. It may benefit 
+	someone else as it did to you!
+	----------------------------------------
 */
 #include<GCRA.h>
 
 GCRA::GCRA(void)
 {
-s_pin_0=0;
-s_pin_1=1;
-s_pin_2=2;
-s_pin_3=3;
-s_pin_4=4;
-s_pin_5=5;
-s_pin_6=6;
+	s_pin_0=0;
+	s_pin_1=1;
+	s_pin_2=2;
+	s_pin_3=3;
+	s_pin_4=4;
+	s_pin_5=5;
+	s_pin_6=6;
 }
 
 GCRA::GCRA(int pin_0,int pin_1,int pin_2,int pin_3,int pin_4,int pin_5,int pin_6)
 {
-s_pin_0=pin_0;
-s_pin_1=pin_1;
-s_pin_2=pin_2;
-s_pin_3=pin_3;
-s_pin_4=pin_4;
-s_pin_5=pin_5;
-s_pin_6=pin_6;
+	s_pin_0=pin_0;
+	s_pin_1=pin_1;
+	s_pin_2=pin_2;
+	s_pin_3=pin_3;
+	s_pin_4=pin_4;
+	s_pin_5=pin_5;
+	s_pin_6=pin_6;
 }
 
 void GCRA::cfg(int servo_dps,int servo_delay,int servo_offset)
 {
-cfg_dps=servo_dps;
-cfg_delay=servo_delay;
-cfg_base_offset=servo_offset;
+	cfg_dps=servo_dps;
+	cfg_delay=servo_delay;
+	cfg_base_offset=servo_offset;
 }
 
 void GCRA::servocfg(int std_min,int std_max,int mini_min,int mini_max)
 {
-std_us_min=std_min;
-std_us_max=std_max;
-mini_us_min=mini_min;
-mini_us_max=mini_max;
+	std_us_min=std_min;
+	std_us_max=std_max;
+	mini_us_min=mini_min;
+	mini_us_max=mini_max;
 }
 
 void GCRA::init(int a_0,int a_1,int a_2,int a_3,int a_4,int a_5,int a_6)
 {
-servo_0.attach(s_pin_0,std_us_min,std_us_max);
-servo_0.write(a_0);delay(cfg_delay);
-
-servo_1.attach(s_pin_1,std_us_min,std_us_max);
-servo_1.write(a_1);delay(cfg_delay);
-
-servo_2.attach(s_pin_2,std_us_min,std_us_max);
-servo_2.write(a_2);delay(cfg_delay);
-
-servo_3.attach(s_pin_3,std_us_min,std_us_max);
-servo_3.write(a_3);delay(cfg_delay);
-
-servo_4.attach(s_pin_4,std_us_min,std_us_max);
-servo_4.write(a_4);delay(cfg_delay);
-
-servo_5.attach(s_pin_5,mini_us_min,mini_us_max);
-servo_5.write(a_5);delay(cfg_delay);
-
-servo_6.attach(s_pin_6,mini_us_min,mini_us_max);
-servo_6.write(a_6);delay(cfg_delay);
+	servo_0.attach(s_pin_0,std_us_min,std_us_max);
+	servo_0.write(a_0);delay(cfg_delay);
+	
+	servo_1.attach(s_pin_1,std_us_min,std_us_max);
+	servo_1.write(a_1);delay(cfg_delay);
+	
+	servo_2.attach(s_pin_2,std_us_min,std_us_max);
+	servo_2.write(a_2);delay(cfg_delay);
+	
+	servo_3.attach(s_pin_3,std_us_min,std_us_max);
+	servo_3.write(a_3);delay(cfg_delay);
+	
+	servo_4.attach(s_pin_4,std_us_min,std_us_max);
+	servo_4.write(a_4);delay(cfg_delay);
+	
+	servo_5.attach(s_pin_5,mini_us_min,mini_us_max);
+	servo_5.write(a_5);delay(cfg_delay);
+	
+	servo_6.attach(s_pin_6,mini_us_min,mini_us_max);
+	servo_6.write(a_6);delay(cfg_delay);
 }
 
-void GCRA::actuate(armpart arm_part,int arm_angle)
+void GCRA::actuate(int arm_part,int arm_angle,bool inv)
 {
-switch(arm_part)
-{
-
-case 0:
-
-break;
-
-case 1:
-
-break;
-
-case 2:
-
-break;
-
-case 3:
-
-break;
-
-case 4:
-
-break;
-
-case 5:
-
-break;
-
-}
-}
+	int set_angle=arm_angle;	//angle to set
+	int cur_angle;				//current angle
+	
+	switch(arm_part)
+	{
+		
+		case 0:
+		if(inv)
+		{
+			set_angle=180-set_angle;
+		}
+		cur_angle=servo_0.read();
+		if(set_angle<cur_angle)
+		{
+			for(int i=cur_angle;i>=set_angle;i-=cfg_dps)
+			{
+				servo_0.write(i);
+				delay(cfg_delay);
+			}
+		}
+		else
+		{
+			for(int i=cur_angle;i<=set_angle;i+=cfg_dps)
+			{
+				servo_0.write(i);
+				delay(cfg_delay);
+			}
+		}		
+		break;
+		
+		case 1:
+		
+		break;
+		
+		case 2:
+		
+		break;
+		
+		case 3:
+		
+		break;
+		
+		case 4:
+		
+		break;
+		
+		case 5:
+		
+		break;
+		
+	}
+	}
--- libraries/GCRA/GCRA.h
+++ libraries/GCRA/GCRA.h
@@ -1,52 +1,55 @@
 /*
-----------------------------------------
-https://github.com/sinisterdev/GCRA_MKI
-----------------------------------------
-S1N1S73RDevelopers-03/10/2015
-I invest time and resources providing 
-this open source code, Please provide 
-proper citations if you plan use it in 
-any of your projects. It may benefit 
-someone else as it did to you!
-----------------------------------------
+	----------------------------------------
+	https://github.com/sinisterdev/GCRA_MKI
+	----------------------------------------
+	S1N1S73RDevelopers-03/10/2015
+	I invest time and resources providing 
+	this open source code, Please provide 
+	proper citations if you plan use it in 
+	any of your projects. It may benefit 
+	someone else as it did to you!
+	----------------------------------------
 */
 //note D:X means X is default value
 #ifndef _GCRA_H_
-#define _GCRA_H_
-#include<Arduino.h>
-#include<Servo.h>
-
-class GCRA
-{
-private:
-
-//datatypes
-Servo servo_0,servo_1,servo_2,servo_3,servo_4,servo_5,servo_6;
-int s_pin_0,s_pin_1,s_pin_2,s_pin_3,s_pin_4,s_pin_5,s_pin_6;
-int std_us_min,std_us_max,mini_us_min,mini_us_max,cfg_dps,cfg_delay,cfg_base_offset;
-
-public:
-
-//datatypes
-enum armpart{waist,shoulder,elbow,wrist_p,wrist_r,tool};	//parts of robotic arm. Note:wrist_p=wrist pitch,wrist_r=wrist roll in actuate()
-
-//methods
-GCRA(void);	//simply creating an object will set pins of servo0->6 to default arduino pins(D:pin 0 to pin 6)
-
-GCRA(int,int,int,int,int,int,int);	//object parameters initialize servo pins for servo0->6(5(standard)+2(mini) servos)
-
-void cfg(int servo_dps=1,int servo_delay=10,int servo_offset=0);	/*(servo deg/step(D:1 degree),delay in milliseconds(D:10ms),
-																	offset servo_2 by given value(in degrees)to match servo_1 as
-																	they share same DOF (D:0 degrees))*/
-																	
-void servocfg(int std_min=544,int std_max=2400,int mini_min=544,int mini_max=2400);	/*configure servo pulse width in micro seconds ;
-																					init(standard servo min,standard servo max,mini servo min,
-																					mini servo max)(D:544,2500 for both)*/
-																					
-void init(int,int,int,int,int,int,int);	//initialize with 7 predefined angles for robotic arm
-
-void actuate(armpart,int);	//actuate(part to actuate,angle to snap to)
-
-};
-
+	#define _GCRA_H_
+	#include<Arduino.h>
+	#include<Servo.h>
+//Literals
+#define waist 0	//parts of robotic arm. Note:wrist_p=wrist pitch,wrist_r=wrist roll in actuate()
+#define shoulder 1
+#define elbow 2
+#define wrist_p 3
+#define wrist_r 4
+#define tool 5
+//Class
+	class GCRA
+	{
+		private:
+		
+		//datatypes
+		Servo servo_0,servo_1,servo_2,servo_3,servo_4,servo_5,servo_6;
+		int s_pin_0,s_pin_1,s_pin_2,s_pin_3,s_pin_4,s_pin_5,s_pin_6;
+		int std_us_min,std_us_max,mini_us_min,mini_us_max,cfg_dps,cfg_delay,cfg_base_offset;
+		
+		public:
+		//methods
+		GCRA(void);	//simply creating an object will set pins of servo0->6 to default arduino pins(D:pin 0 to pin 6)
+		
+		GCRA(int,int,int,int,int,int,int);	//object parameters initialize servo pins for servo0->6(5(standard)+2(mini) servos)
+		
+		void cfg(int servo_dps=1,int servo_delay=10,int servo_offset=0);	/*(servo deg/step(D:1 degree),delay in milliseconds(D:10ms),
+																			offset servo_2 by given value(in degrees)to match servo_1 as
+																			they share same DOF (D:0 degrees))*/
+		
+		void servocfg(int std_min=544,int std_max=2400,int mini_min=544,int mini_max=2400);	/*configure servo pulse width in micro seconds ;
+																							init(standard servo min,standard servo max,mini servo min,
+																							mini servo max)(D:544,2500 for both)*/
+		
+		void init(int,int,int,int,int,int,int);	//initialize with 7 predefined angles for robotic arm
+		
+		void actuate(int,int,bool inv=0);	/*actuate(part to actuate,angle to snap to,invert angles) 
+												note:Flipped a servo?..No problem use: actuate(x.., y.., 1)*/
+		
+	};
 #endif

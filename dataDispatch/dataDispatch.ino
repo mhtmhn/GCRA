a9aa327617f76cec8e1323ff20d0aa6640f0e8a9
Mohit Mohan <mht.mhn@gmail.com>
2015-03-18 23:54:47 +0530

    Update Transmitter, Reciever Code
    Tested Working

--- dataDispatch/dataDispatch.ino
+++ dataDispatch/dataDispatch.ino
@@ -1,70 +1,192 @@
 //(c)S1NIS73R
 //#define dbug
+
+//Arduino
+#include "Wire.h"
 #include <SPI.h>
+
+//RF24 by TMRh20 https://tmrh20.github.io/RF24
 #include <RF24.h>
+
+//MPU-6050, I2Cdev Library by Jeff Rowberg https://github.com/jrowberg/i2cdevlib
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+MPU6050 mpu;	//MPU6050 mpu(0x69); // <-- use for AD0 high
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// MPU orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+//ISR
+volatile bool mpuInterrupt = false;	// indicates whether MPU interrupt pin has gone high
+void dmpDataReady() 
+{
+mpuInterrupt = true;
+}
 
 //nRF24L01+
 const uint64_t address = 0xABCDABCD71LL;
-RF24 radio(9,8);//(CE,CSN)
-boolean sent;
-struct packet
-{
-  float ax,ay,az;
-  int gx,gy,gz,fx,fy,fz;
-}dispatch;
-
-void setup() 
-{
-  //Debug
-  #ifdef dbug
-  Serial.begin(115200);
-  #endif
-  
-  //Flex
-  dispatch.fx=0;
-  dispatch.fy=0;
-  dispatch.fz=0;
-  
-  //nRF24L01+
-  radio.begin();
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setDataRate(RF24_2MBPS);
-  radio.setAutoAck(1);
-  radio.setRetries(2,15);
-  radio.setCRCLength(RF24_CRC_8);
-  radio.openWritingPipe(address);
-}
-
-void loop() 
-{
-  //Flex
-  dispatch.fx=analogRead(A2);
-  dispatch.fy=analogRead(A0);
-  dispatch.fz=analogRead(A1);
-  
-  //nRF24L01+
-  radio.stopListening();
-  sent=radio.writeFast( &dispatch, sizeof(dispatch) );
-  
-  //Debug
-  #ifdef dbug
-     if (!sent)
-     {
-       Serial.println("Null Dispatch");
-     }
-     else
-     {
-       Serial.print("Dispatched Data:");
-       Serial.print(dispatch.ax); Serial.print(",");
-       Serial.print(dispatch.ay); Serial.print(",");
-       Serial.print(dispatch.az); Serial.print(",");
-       Serial.print(dispatch.gx); Serial.print(",");
-       Serial.print(dispatch.gy); Serial.print(",");
-       Serial.print(dispatch.gz); Serial.print(",");
-       Serial.print(dispatch.fx); Serial.print(",");
-       Serial.print(dispatch.fy); Serial.print(",");
-       Serial.print(dispatch.fz);
-       Serial.println();
-     }
-  #endif
-}
+	RF24 radio(9,8);//(CE,CSN)
+	boolean sent;
+	struct packet
+	{
+		float y,p,r;
+		int fx,fy,fz;
+	}dispatch;
+	
+	void setup() 
+	{
+		#ifdef dbug
+		Serial.begin(115200);
+		while (!Serial); // wait for Leonardo enumeration, others continue immediately
+		#endif
+		
+		//Flex
+		dispatch.fx=0;
+		dispatch.fy=0;
+		dispatch.fz=0;
+		
+		//nRF24L01+
+		radio.begin();
+		radio.setPALevel(RF24_PA_MAX);
+		radio.setDataRate(RF24_2MBPS);
+		radio.setAutoAck(1);
+		radio.setRetries(2,15);
+		radio.setCRCLength(RF24_CRC_8);
+		radio.openWritingPipe(address);
+		
+		Wire.begin();
+		TWBR = 24;
+		boolean error = true;
+		while (error) 
+		{
+			Wire.beginTransmission(104);
+			error = Wire.endTransmission(); // if error = 0, we are properly connected
+			if (error) 
+			{ // if we aren't properly connected, try connecting again and loop
+				#ifdef dbug
+				Serial.println("Not properly connected to I2C, trying again");
+				#endif
+				Wire.begin();
+				TWBR = 24; // 400kHz I2C clock
+			}
+		}
+		#ifdef dbug
+		Serial.println("Properly connected to I2C");
+		// initialize device
+		Serial.println(F("Initializing I2C devices..."));
+		#endif
+		mpu.initialize();
+		#ifdef dbug
+		// verify connection
+		Serial.println(F("Testing device connections..."));
+		Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+		// load and configure the DMP
+		Serial.println(F("Initializing DMP..."));
+		#endif
+		devStatus = mpu.dmpInitialize();
+		
+		if (devStatus == 0) 
+		{
+			// turn on the DMP, now that it's ready
+			#ifdef dbug
+			Serial.println(F("Enabling DMP..."));
+			#endif
+			mpu.setDMPEnabled(true);
+			
+			// enable Arduino interrupt detection
+			#ifdef dbug
+			Serial.println(F("Enabling interrupt detection (Arduino external int.4)..."));
+			#endif
+			
+			attachInterrupt(4, dmpDataReady, RISING);
+			mpuIntStatus = mpu.getIntStatus();
+			
+			// set our DMP Ready flag so the main loop() function knows it's okay to use it
+			#ifdef dbug
+			Serial.println(F("DMP ready! Waiting for first interrupt..."));
+			#endif
+			dmpReady = true;
+			
+			// get expected DMP packet size for later comparison
+			packetSize = mpu.dmpGetFIFOPacketSize();
+		} 
+		#ifdef dbug
+		else 
+		{
+			// ERROR!
+			// 1 = initial memory load failed
+			// 2 = DMP configuration updates failed
+			// (if it's going to break, usually the code will be 1)
+			Serial.print(F("DMP Initialization failed (code "));
+			Serial.print(devStatus);
+			Serial.println(F(")"));
+		}
+		#endif
+	}
+	
+	void loop() 
+	{
+		if (!dmpReady) return;
+		
+		while (!mpuInterrupt && fifoCount < packetSize) 
+		{	
+			boolean error = true;
+			while (error) 
+			{
+				Wire.beginTransmission(104);
+				error = Wire.endTransmission(); // if error = 0, we are properly connected
+				if (error) 
+				{ // if we aren't properly connected, try connecting again and loop
+					Wire.begin();
+					TWBR = 24; // 400kHz I2C clock
+				}
+			}
+			//Flex
+			dispatch.fx=analogRead(A0);
+			dispatch.fy=analogRead(A1);
+			dispatch.fz=analogRead(A2);
+			
+			//nRF24L01+
+			radio.stopListening();
+			sent=radio.writeFast( &dispatch, sizeof(dispatch) );
+		}
+		mpuInterrupt = false;
+		mpuIntStatus = mpu.getIntStatus();
+		fifoCount = mpu.getFIFOCount();
+		
+		if ((mpuIntStatus & 0x10) || fifoCount == 1024) 
+		{
+			#ifdef dbug
+			Serial.println(F("FIFO overflow!"));
+			Serial.println(F("Purging!"));	
+			#endif
+			mpu.resetFIFO();		
+		} 
+		else if (mpuIntStatus & 0x02) 
+		{
+			while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+			mpu.getFIFOBytes(fifoBuffer, packetSize);
+			fifoCount -= packetSize;
+			mpu.dmpGetQuaternion(&q, fifoBuffer);
+			mpu.dmpGetGravity(&gravity, &q);
+			mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+			dispatch.y=ypr[0];
+			dispatch.p=ypr[1];
+			dispatch.r=ypr[2];
+			#ifdef dbug
+			Serial.print(dispatch.y); Serial.print(" ");
+			Serial.print(dispatch.p); Serial.print(" ");
+			Serial.println(dispatch.r);
+			#endif
+		}
+	}
--- dataIntercept/dataIntercept.ino
+++ dataIntercept/dataIntercept.ino
@@ -1,6 +1,10 @@
 //(c)S1NIS73R
-//#define dbug
+#define dbug
+
+//Arduino
 #include <SPI.h>
+
+//RF24 by TMRh20 https://tmrh20.github.io/RF24
 #include <RF24.h>
 
 //nRF24L01+
@@ -8,8 +12,8 @@ const uint64_t address = 0xABCDABCD71LL;
 RF24 radio(9,8);//(CE,CSN)
 struct packet
 {
-  float ax,ay,az;
-  int gx,gy,gz,fx,fy,fz;
+  float y,p,r;
+  int fx,fy,fz;
 }intercept;
 
 void setup() 
@@ -39,12 +43,9 @@ void loop()
     
   //Debug
   #ifdef dbug      
-      Serial.print(intercept.ax); Serial.print(" ");
-      Serial.print(intercept.ay); Serial.print(" ");
-      Serial.print(intercept.az); Serial.print(" ");
-      Serial.print(intercept.gx); Serial.print(" ");
-      Serial.print(intercept.gy); Serial.print(" ");
-      Serial.print(intercept.gz); Serial.print(" ");
+      Serial.print(intercept.y); Serial.print(" ");
+      Serial.print(intercept.p); Serial.print(" ");
+      Serial.print(intercept.r); Serial.print(" ");
       Serial.print(intercept.fx); Serial.print(" ");
       Serial.print(intercept.fy); Serial.print(" ");
       Serial.print(intercept.fz); Serial.print(" ");
